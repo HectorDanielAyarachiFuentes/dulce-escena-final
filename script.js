16187adffbafdbcaf24bb153c1b7c4ce35f3fd11
Ayarachi Fuentes Hector Daniel <mp4o@hotmail.com>
2026-05-03 22:01:53 -0300

    feat: add drawing dog element and background music with autoplay handling

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+// ====== MÚSICA ======
+const musica = document.getElementById('musica');
+musica.play().catch(() => {
+    const iniciar = () => {
+        musica.play();
+        document.removeEventListener('click', iniciar);
+        document.removeEventListener('touchstart', iniciar);
+    };
+    document.addEventListener('click', iniciar);
+    document.addEventListener('touchstart', iniciar);
+});
